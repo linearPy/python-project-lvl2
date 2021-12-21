ea1af8e1189605bd8db66c2b5660e4b6015150a5
linearPy <starkov.yegor2020@gmail.com>
2021-12-22 02:55:56 +0600

    add gendiff package

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(prog='gendiff', description='Generate diff')
+
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+
+    parser.parse_args()
+
+if __name__ == '__main__':
+    main()
+
+

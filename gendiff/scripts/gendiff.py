14857a50614862fbcbebb72ef9e3cb57d279a78b
linearPy <starkov.yegor2020@gmail.com>
2021-12-22 04:38:28 +0600

    add optional arg

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -9,8 +9,10 @@ def main():
 
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
 
     parser.parse_args()
+
 
 if __name__ == '__main__':
     main()

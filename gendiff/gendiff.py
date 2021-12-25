f67b563cc0da2445011504df5d59b1491e22d9da
linearPy <starkov.yegor2020@gmail.com>
2021-12-25 06:31:39 +0600

    refactor func

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,23 +1,22 @@
-import json
+from gendiff.parser import deserialize
 
 
 def generate_diff(first_path, second_path):
-    with open(first_path) as file1, open(second_path) as file2:
-        data1 = json.load(file1)
-        data2 = json.load(file2)
+    data1 = deserialize(first_path)
+    data2 = deserialize(second_path)
 
-        keys = sorted(data1.keys() | data2.keys())
-        result = []
+    keys = sorted(data1.keys() | data2.keys())
+    result = []
 
-        for key in keys:
-            if key in data1 and key not in data2:
-                result.append(f'  - {key}: {data1[key]}')
-            elif key in data2 and key not in data1:
-                result.append(f'  + {key}: {data2[key]}')
-            elif data1[key] == data2[key]:
-                result.append(f'    {key}: {data1[key]}')
-            else:
-                result.append(f'  - {key}: {data1[key]}')
-                result.append(f'  + {key}: {data2[key]}')
+    for key in keys:
+        if key in data1 and key not in data2:
+            result.append(f'  - {key}: {data1[key]}')
+        elif key in data2 and key not in data1:
+            result.append(f'  + {key}: {data2[key]}')
+        elif data1[key] == data2[key]:
+            result.append(f'    {key}: {data1[key]}')
+        else:
+            result.append(f'  - {key}: {data1[key]}')
+            result.append(f'  + {key}: {data2[key]}')
 
     return '{\n' + '\n'.join(result) + '\n}\n'

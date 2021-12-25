5f6ceb24465ec9574508786287c42c6185cf7437
linearPy <starkov.yegor2020@gmail.com>
2021-12-25 06:32:54 +0600

    tests/test_gendiff.py
    add new assert for YAML

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,7 +3,9 @@ from gendiff.gendiff import generate_diff
 
 FLAT_JSON_1 = 'tests/fixtures/flat_1.json'
 FLAT_JSON_2 = 'tests/fixtures/flat_2.json'
-FLAT_JSON_ANSWER = 'tests/fixtures/flat_json_answer'
+FLAT_YAML_1 = 'tests/fixtures/flat_1.yml'
+FLAT_YAML_2 = 'tests/fixtures/flat_2.yml'
+FLAT_ANSWER = 'tests/fixtures/flat_answer'
 
 
 def get_expected_text(path):
@@ -15,4 +17,7 @@ def get_expected_text(path):
 
 def test_generate_diff():
     assert generate_diff(FLAT_JSON_1, FLAT_JSON_2) == \
-           get_expected_text(FLAT_JSON_ANSWER)
+           get_expected_text(FLAT_ANSWER)
+
+    assert generate_diff(FLAT_YAML_1, FLAT_YAML_2) == \
+           get_expected_text(FLAT_ANSWER)
